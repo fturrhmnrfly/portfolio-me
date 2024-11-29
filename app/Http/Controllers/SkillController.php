6a6e9b21bb229ab616722a037d4945274ca1919e
Nama Anda <email@example.com>
2024-11-29 08:45:38 +0700

    init: initialize commit

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Skill;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
     public function skill(){
-        return view('skill');
+        $skills = Skill::all();
+        return view('skill', compact('skills'));
     }
 }
--- resources/views/skill.blade.php
+++ resources/views/skill.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Skills</title>
-</head>
-<body>
-    @include('navbar')
-    <h1>My Skills</h1>
-    <p>This is the skills page.</p>
-</body>
-</html>
+<x-layout>
+    @foreach ( $skills as $skill )
+        {{ $skill->title }}
+        {{ $skill->description }}
+    @endforeach
+</x-layout>
